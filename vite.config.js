86bf08a876e75b4840d7784d1d570a5a6d37b52e
Boyi <161673296+seraphimboyi@users.noreply.github.com>
2024-06-25 19:21:54 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "my-website", //這邊換成你的專案名
+  base: "my-website",
 })
