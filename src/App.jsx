3e2c55a78ca8cb307ce7e70c95eb3c7703eb8605
Boyi <161673296+seraphimboyi@users.noreply.github.com>
2024-06-25 19:26:23 +0800

    modify: change something.

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Hello world
         </p>
       </div>
       <p className="read-the-docs">
